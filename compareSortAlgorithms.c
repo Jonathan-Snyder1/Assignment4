cb27523ba4d743e28bc62e77fcce0b3957e4b6d2
Jonathan-Snyder1 <jonathan@flsnyder.com>
2023-03-21 12:12:42 -0400

    sort algorithms

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -13,19 +13,16 @@ void merge(int pData[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 =  r - m;
 
-    // create temp arrays
     int L[n1], R[n2];
 
-    // Copy data to temp arrays L[] and R[]
     for (i = 0; i < n1; i++)
         L[i] = pData[l + i];
     for (j = 0; j < n2; j++)
         R[j] = pData[m + 1+ j];
 
-    // Merge the temp arrays back into pData[l..r]
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    i = 0;
+    j = 0;
+    k = l;
     while (i < n1 && j < n2)
     {
         if (L[i] <= R[j])
@@ -41,7 +38,6 @@ void merge(int pData[], int l, int m, int r)
         k++;
     }
 
-    // Copy the remaining elements of L[], if there are any
     while (i < n1)
     {
         pData[k] = L[i];
@@ -49,24 +45,22 @@ void merge(int pData[], int l, int m, int r)
         k++;
     }
 
-    // Copy the remaining elements of R[], if there are any
     while (j < n2)
     {
         pData[k] = R[j];
         j++;
         k++;
     }
-    extraMemoryAllocated += sizeof(int)*(n1+n2);
 }
+
+
 
 void mergeSort(int pData[], int l, int r)
 {
     if (l < r)
     {
-        // Same as (l+r)/2, but avoids overflow for large l and r
-        int m = l+(r-l)/2;
+        int m = (l+r)/2;
 
-        // Sort first and second halves
         mergeSort(pData, l, m);
         mergeSort(pData, m+1, r);
 
